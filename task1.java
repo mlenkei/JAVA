992ed4316051632b3edcae9a4528cd105f7a8bbb
Leonid Maslov <mlenkei@mail.ru>
2023-06-22 05:03:49 +0300

    task1

---
+++ task1.java
@@ -0,0 +1,9 @@
+public class Test {// объявляется класс Test
+
+    public static void main(String[] args) {// так включаются программы,
+                                            // просто запомнить
+
+        System.out.println("Hello world");
+        System.out.println("5 раз");
+    }
+}
